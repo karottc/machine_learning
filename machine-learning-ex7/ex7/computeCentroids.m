84f491b8039bd855307ce608d2c42a817d43f174
karottc <karottc@gmail.com>
2016-09-16 18:24:09 +0800

    完成week 8 的变成作业。

--- machine-learning-ex7/ex7/computeCentroids.m
+++ machine-learning-ex7/ex7/computeCentroids.m
@@ -1,7 +1,7 @@
 function centroids = computeCentroids(X, idx, K)
-%COMPUTECENTROIDS returs the new centroids by computing the means of the 
+%COMPUTECENTROIDS returs the new centroids by computing the means of the
 %data points assigned to each centroid.
-%   centroids = COMPUTECENTROIDS(X, idx, K) returns the new centroids by 
+%   centroids = COMPUTECENTROIDS(X, idx, K) returns the new centroids by
 %   computing the means of the data points assigned to each centroid. It is
 %   given a dataset X where each row is a single data point, a vector
 %   idx of centroid assignments (i.e. each entry in range [1..K]) for each
@@ -27,14 +27,16 @@ centroids = zeros(K, n);
 %
 
 
-
-
-
-
+for i = 1:K
+    c_i = idx==i;
+    n_i = sum(c_i);
+    c_i_matrix = repmat(c_i,1,n);
+    Xci = X .* c_i_matrix;
+    centroids(i,:) = sum(Xci) ./ n_i;
+end
 
 
 % =============================================================
 
 
 end
-
